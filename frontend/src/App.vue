d5b5570d8e4c606f1000a9408c1da4bd97ec98f5
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-10-04 20:49:16 +0200

    Mise à jour de l'App et des components
    Suite au modifs du frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <nav>
+      <appHeader></appHeader>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import appHeader from './views/Header.vue'
+
+export default {
+  components: {
+    appHeader
+  },
+    beforeUpdate() {
+      console.log('beforeupdate: ', this.$store.state.avatarUser);
+    this.$store.state.tokenToCheck = this.$ls.get('token');
+    this.$store.state.userId = this.$ls.get('userId');
+    this.$store.commit('CHECK_TOKEN');
+  },
+    beforeCreate() {
+    console.log('beforecreated: ', this.$store.state.avatarUser);
+    this.$store.state.tokenToCheck = this.$ls.get('token');
+    this.$store.state.userId = this.$ls.get('userId');
+    this.$store.dispatch('getInfos');
+    this.$store.commit('CHECK_TOKEN');
+  },
+  beforeMount() {
+    console.log('beforemount: ', this.$store.state.avatarUser);
+    this.$store.dispatch('getInfos');
+  },
+  created() {
+    console.log('created: ', this.$store.state.avatarUser);
+    console.log('created');
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import './sass/main.scss';
+
+* {
+  padding:0;
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ frontend/src/components/DashBoardPosts.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+
+    <h3> Posts signalés </h3>
+
+    <div class="container">
+        <Post v-for="(post, postIndex) in posts" :key="post.id" :authorId="post.authorId" :message="post.message" :image="post.image" :index="postIndex" :id="post.id" :isFlagged="post.isFlagged"></Post>
+    </div>
+    
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import Post from '../components/post.vue'
+
+export default {
+    data() {
+        return {
+            posts: [],
+        }
+    },
+    methods: {
+        getFlaggedPosts() {
+            axios.get('http://localhost:3000/dashBoard/flaggedPosts', { headers: {
+                'Authorization': `token ${this.$store.state.tokenToCheck}`
+                }} )
+            .then(response => {
+                this.posts = response.data.resultat; 
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    components: {
+        Post
+    },
+    mounted() {
+        this.getFlaggedPosts();
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+h3 
+{
+    margin-left: auto;
+    margin-right: auto;
+    border-bottom: 5px inset #fd2d01;
+    width: 300px;
+}
+
+a
+{
+    color: #fd2d01;
+}
+
+a:hover
+{
+    color: hsl(10, 100%, 74%);
+}
+
+</style>
